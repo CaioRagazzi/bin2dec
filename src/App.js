e9296e3597124ff97d94a3348812234307a16dd7
Caio <ca.ragazzi@gmail.com>
2020-02-18 23:29:24 -0300

    binario to dec

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [binaryNumber, setBinaryNumber] = useState("")
+
+  const handleBinaryChange = (event) => {
+    const isValid = isInputValid(event.target.value)
+
+    if (isValid) {
+      setBinaryNumber(event.target.value)
+    }
+  }
+
+  const isInputValid = (text) => {
+    let typedText = text.charAt(text.length - 1)
+
+    if (typedText === "")
+      return true
+
+    return isNumber(typedText) && isOneOrZero(typedText) && isLengthGreaterThanEigth(text)
+  }
+
+  const isLengthGreaterThanEigth = (input) => {
+    let maxLengthPermit = 8
+    if (input.length > maxLengthPermit) {
+      alert('Max input exceeded!')
+    } else {
+      return true
+    }
+  }
+
+  const isNumber = (number) => {
+    if (!isNaN(number)) {
+      return true
+    } else {
+      alert('Not a number!')
+    }
+  }
+
+  const isOneOrZero = (number) => {
+    if (parseInt(number) === 0 || parseInt(number) === 1) {
+      return true
+    } else {
+      alert('Input must be 0 or 1!')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleBinaryChange} value={binaryNumber} ></input>
+      <h1> { binaryNumber } </h1>
     </div>
   );
 }
