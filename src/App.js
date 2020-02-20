59109eb7ee42ec305fd5e0a52f1927e59403b0a7
Caio <ca.ragazzi@gmail.com>
2020-02-19 22:54:00 -0300

    migrado para vuejs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [binaryNumber, setBinaryNumber] = useState("")
+  const [decimalNumber, setDecimalNumber] = useState(0)
 
   const handleBinaryChange = (event) => {
     const isValid = isInputValid(event.target.value)
 
     if (isValid) {
       setBinaryNumber(event.target.value)
+      calcDecimal()
     }
   }
 
@@ -45,10 +47,26 @@ function App() {
     }
   }
 
+  const calcDecimal = () => {
+    setDecimalNumber(0)
+
+    var total = 0
+    
+    for (let i = binaryNumber.length - 1; i >= 0; i--) {
+      
+      let calc = parseInt(binaryNumber[i]) * Math.pow(2, total)
+      console.log(calc);
+      console.log(decimalNumber);
+      setDecimalNumber(decimalNumber + calc)
+      total++
+    }
+  }
+
   return (
     <div className="App">
       <input type="text" onChange={handleBinaryChange} value={binaryNumber} ></input>
-      <h1> { binaryNumber } </h1>
+      <h1> {decimalNumber} </h1>
+      <h1> {binaryNumber} </h1>
     </div>
   );
 }
